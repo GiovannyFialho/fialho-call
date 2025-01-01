19d0442ad6c78a60a73c02c93ba218f8c709497a
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-01 15:14:35 -0300

    comecando estruturacao da home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
 "use client";
 
-import { Heading } from "@ignite-ui/react";
+import { Heading, Text } from "@ignite-ui/react";
+import Image from "next/image";
+
+import previewImage from "@/app/assets/app-preview.png";
+
+import { Container, Hero, Preview } from "@/app/home/styles";
 
 export default function Home() {
-  return <Heading as="h1">Hello World</Heading>;
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  );
 }
