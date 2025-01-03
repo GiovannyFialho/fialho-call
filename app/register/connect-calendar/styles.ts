397a16f1baba730d51477f6bf795504b50600ff9
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-03 15:41:29 -0300

    adicionando validacoes de login

--- app/register/connect-calendar/styles.ts
+++ app/register/connect-calendar/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@ignite-ui/react";
+import { Box, styled, Text } from "@ignite-ui/react";
 
 export const ConnectBox = styled(Box, {
   marginTop: "$6",
@@ -10,8 +10,15 @@ export const ConnectItem = styled("div", {
   display: "flex",
   alignItems: "center",
   justifyContent: "space-between",
+
   border: "1px solid $gray600",
   padding: "$4 $6",
   borderRadius: "$md",
-  marginBottom: "$2",
+
+  marginBottom: "$4",
+});
+
+export const AuthError = styled(Text, {
+  color: "#f75a68",
+  marginBottom: "$4",
 });
