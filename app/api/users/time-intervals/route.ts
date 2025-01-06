78254a9f4f5170ba86ee89e9f989762882444d0c
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-06 15:11:27 -0300

    salvando no banco os dados de preferencia de horario

--- app/api/users/time-intervals/route.ts
+++ app/api/users/time-intervals/route.ts
@@ -1,7 +1,19 @@
 import { getServerSession } from "next-auth";
 import { type NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { prisma } from "@/app/lib/prisma";
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    })
+  ),
+});
 
 export async function POST(request: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -10,9 +22,39 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  const body = await request.json();
+  try {
+    const body = await request.json();
+    const { intervals } = timeIntervalsBodySchema.parse(body);
 
-  return NextResponse.json({ session, body });
+    try {
+      await Promise.all(
+        intervals.map((interval) => {
+          return prisma.userTimeInterval.create({
+            data: {
+              week_day: interval.weekDay,
+              time_start_in_minutes: interval.startTimeInMinutes,
+              time_end_in_minutes: interval.endTimeInMinutes,
+              user_id: session.user.id,
+            },
+          });
+        })
+      );
+
+      return NextResponse.json({ status: 201 });
+    } catch (error) {
+      console.error("Prisma error:", error);
+
+      return NextResponse.json(
+        { error: "Database operation failed", details: error },
+        { status: 500 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Invalid data", details: error },
+      { status: 400 }
+    );
+  }
 }
 
 export function OPTIONS() {
