03e01958d0a0d21a057f7ab8473ba8c5daa19f87
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-07 15:48:40 -0300

    criacao do formulario de disponibilidade

--- app/schedule/[username]/ScheduleForm/index.tsx
+++ app/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,6 @@
-import { CalendarStep } from "@/app/schedule/[username]/ScheduleForm/CalendarStep";
+// import { CalendarStep } from "@/app/schedule/[username]/ScheduleForm/CalendarStep";
+import { ConfirmStep } from "@/app/schedule/[username]/ScheduleForm/ConfirmStep";
 
 export function ScheduleForm() {
-  return <CalendarStep />;
+  return <ConfirmStep />;
 }
